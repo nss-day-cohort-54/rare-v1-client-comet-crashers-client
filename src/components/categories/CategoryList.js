b703babe2402522a3fd36127fa010cbb8b9b0feb
Jake Brooks <jacobdbrooks1998@gmail.com>
2022-04-26 15:39:22 -0500

    created function to display categories and have route

--- src/components/categories/CategoryList.js
+++ src/components/categories/CategoryList.js
@@ -4,13 +4,20 @@ import { getCategories } from "./CategoryManager";
 export const CategoryList = () => {
     const [categories, setCategories] = useState([])
 
-    const getAllCategories = () => {
+    useEffect(() => {
         getCategories().then(categoriesData => setCategories(categoriesData))
-      }
+    }, [])
 
     return (
         <>
-        <h1>Categories</h1>
+        <h1><b>Categories</b></h1>
+        <div className = "categories">
+        {
+            categories.map(category => {
+                return <div>{category.label}</div>
+            })
+        }
+        </div>
         </>
     )
 }
